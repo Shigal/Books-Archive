dd6ebc040fdb6db1536a1e550f8ff6bc14f1be68
Shigal <shiglaws@gmail.com>
2022-12-24 20:16:10 +0530

    primitive cannot be null so long changed to Long

--- src/main/java/com/datajpa/jpaentityrelationship/model/Category.java
+++ src/main/java/com/datajpa/jpaentityrelationship/model/Category.java
@@ -19,7 +19,7 @@ public class Category {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private long id;
+    private Long id;
 
     private String name;
 
--- src/main/java/com/datajpa/jpaentityrelationship/model/Zipcode.java
+++ src/main/java/com/datajpa/jpaentityrelationship/model/Zipcode.java
@@ -17,7 +17,7 @@ public class Zipcode {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private long id;
+    private Long id;
 
     private String name;
 
