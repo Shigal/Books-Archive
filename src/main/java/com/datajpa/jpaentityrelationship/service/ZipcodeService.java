1d5682b807e777b88a8bae9067df18c732607bff
Shigal <shiglaws@gmail.com>
2022-12-24 21:04:21 +0530

    zipcode service created

---
+++ src/main/java/com/datajpa/jpaentityrelationship/service/ZipcodeService.java
@@ -0,0 +1,22 @@
+package com.datajpa.jpaentityrelationship.service;/*
+ *
+ * @author Lawshiga
+ *
+ */
+
+import com.datajpa.jpaentityrelationship.dto.requestdto.CityRequestDto;
+import com.datajpa.jpaentityrelationship.dto.requestdto.ZipcodeRequestDto;
+import com.datajpa.jpaentityrelationship.model.City;
+import com.datajpa.jpaentityrelationship.model.Zipcode;
+
+import java.util.List;
+
+public interface ZipcodeService {
+    public Zipcode addZipcode(ZipcodeRequestDto zipcodeRequestDto);
+    public List<Zipcode> getZipcodes();
+    public Zipcode getZipcode(Long zipcodeId);
+    public Zipcode deleteZipcode(Long zipcodeId);
+    public Zipcode editZipcode(Long zipcodeId, ZipcodeRequestDto zipcodeRequestDto);
+    public Zipcode addCityToZipcode(Long zipcodeId, Long cityId);
+    public Zipcode removeCityFromZipcode(Long zipcodeId);
+}
